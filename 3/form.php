217e4a17c340b1dc6ff3a56c31c55f2d1b9fab55
Daniil-Ketov <90641989+Daniil-Ketov@users.noreply.github.com>
2022-03-27 15:41:05 +0300

    Add files via upload

---
+++ 3/form.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="styles.css">
+	<title>Задание 3</title>
+</head>
+<body>
+  <main>
+    <form action="" method="POST">
+      <ul style="list-style: none;">
+        <li>
+          <label class="field-name">
+            Имя <br>
+          </label>
+          <input name="name" type="text" />
+        </li>
+        <li>
+          <label class="field-name">
+            e-mail <br>
+          </label>
+          <input name="e-mail" type="text" />
+        </li>
+        <li>      
+          <label class="field-name">
+            Год рождения <br>
+          </label>
+          <select name="bdate[]" class="select-dropdown">
+          <?php
+            $options = array();
+            for ($i = 1900; $i <= 2022; $i++) {
+              $options[] = $i;
+            }
+            foreach ($options as $option) {
+            ?>
+              <option value="<?php echo $option;?>">
+                <?php echo $option;?>
+              </option>
+            <?php
+            }
+            ?>
+          </select>
+        </li>
+        <li>
+          <label class="field-name">
+            Пол <br>
+          </label>
+          <input type="radio" name="gender" value="m">
+          <label>
+            Мужской
+          </label>
+          <input type="radio" name="gender" value="w">
+          <label>
+            Женский
+          </label>
+        </li>
+        <li>
+          <label class="field-name">
+            Количество конечностей <br>
+          </label>
+          <input type="radio" name="limbs" value=0>
+          <label>0</label>
+          <input type="radio" name="limbs" value=1>
+          <label>1</label>
+          <input type="radio" name="limbs" value=2>
+          <label>2</label>
+          <input type="radio" name="limbs" value=3>
+          <label>3</label>
+          <input type="radio" name="limbs" value=4>
+          <label>4</label>
+          <input type="radio" name="limbs" value=5>
+          <label>5+</label>
+        </li>
+        <li>
+          <label class="field-name">
+            Сверхспособности <br>
+          </label>
+          <select multiple="true" name="superpower[]" class="select-list">
+              <option value="Бессмертие">Бессмертие</option>
+              <option value="Прохождение сквозь стены">Прохождение сквозь стены</option>
+              <option value="Левитация">Левитация</option>
+          </select>
+        </li>
+        <li>
+          <label class="field-name">
+            Биография <br>
+          </label>
+          <textarea name="bio" cols="30" rows="10" style="resize: none;"></textarea>
+        </li>
+        <li>
+          <input type="checkbox" name="checkbox">
+          <label>
+            С контрактом ознакомлен
+          </label>
+        </li>
+        <li>
+          <input type="submit" class="submit" value="Отправить" />
+        </li>
+      </ul>
+    </form>
+  </main>
+  <footer>
+		(c) Даниил Кетов 2022
+	</footer>
+</body>
+</html>
+
---
+++ 3/index.php
@@ -0,0 +1,86 @@
+<?php
+// Отправляем браузеру правильную кодировку,
+// файл index.php должен быть в кодировке UTF-8 без BOM.
+header('Content-Type: text/html; charset=UTF-8');
+
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+  if (!empty($_GET['save'])) {
+    // Если есть параметр save, то выводим сообщение пользователю.
+    print('Спасибо, результаты сохранены.');
+  }
+  // Включаем содержимое файла form.php.
+  include('form.php');
+  // Завершаем работу скрипта.
+  exit();
+}
+// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
+
+// Проверяем ошибки.
+$errors = FALSE;
+if (empty($_POST['name'])) {
+  print('Заполните имя.<br/>');
+  $errors = TRUE;
+}
+else if (empty($_POST['e-mail'])) {
+  print('Заполните e-mail.<br/>');
+  $errors = TRUE;
+}
+else if (!preg_match("/@/\./", $_POST['e-mail'])) {
+  print('Введите корректный e-mail.<br/>');
+  $errors = TRUE;
+}
+else if (empty($_POST['bdate'])) {
+  print('Заполните дату рождения.<br/>');
+  $errors = TRUE;
+}
+else if (empty($_POST['gender'])) {
+  print('Заполните пол.<br/>');
+  $errors = TRUE;
+}
+else if (empty($_POST['limbs'])) {
+  print('Заполните число конечностей.<br/>');
+  $errors = TRUE;
+}
+else if (empty($_POST['bio'])) {
+  print('Заполните биографию.<br/>');
+  $errors = TRUE;
+}
+else if (empty($_POST['checkbox'])) {
+  print('Согласитесь с контрактом.<br/>');
+  $errors = TRUE;
+}
+
+if ($errors) {
+  // При наличии ошибок завершаем работу скрипта.
+  exit();
+}
+
+// Сохранение в базу данных.
+$name = $_POST['name'];
+$email = $_POST['e-mail'];
+$bdate = $_POST['bdate'];
+$gender = $_POST['gender'];
+$limbs = $_POST['limbs'];
+$superpowers = implode(',', $_POST['superpower']);
+$bio = $_POST['bio'];
+$check = $_POST['checkbox'];
+
+
+$user = 'u47477';
+$pass = '5680591';
+$db = new PDO('mysql:host=localhost;dbname=u47477', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
+// Подготовленный запрос. Не именованные метки.
+try {
+  $stmt = $db->prepare("INSERT INTO application (name) SET name = ?, email = ?, bdate = ?, gender = ?, limbs = ?, superpowers = ?, bio = ?");
+  $stmt -> execute(array($name, $email, $bdate, $gender, $limbs, $superpowers, $bio));
+  $id = $db->lastInsertId();
+  echo "Данные успешно сохранены." . $id;
+}
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
+}
