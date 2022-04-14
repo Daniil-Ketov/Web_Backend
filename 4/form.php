0429dfd60196043287bdb5b0e8d5287805d73af8
Daniil-Ketov <90641989+Daniil-Ketov@users.noreply.github.com>
2022-04-14 22:57:46 +0300

    Update form.php

--- 4/form.php
+++ 4/form.php
@@ -36,7 +36,7 @@
             }
             foreach ($options as $option) {
             ?>
-              <option value=<?php echo $option;?>>
+              <option value=<?php echo $option;?> <?php if ($values['bdate'] == $option) {print 'selected';} ?> >
                 <?php echo $option;?>
               </option>
             <?php
@@ -49,11 +49,11 @@
             Пол <br>
 			      <p <?php if ($errors['gender']) {print 'class="error"';} ?> > <?php if ($messages['gender']) print $messages['gender']; ?> </p>
           </label>
-          <input type="radio" name="gender" value="m" <?php if ($errors['gender']) {print 'class="error"';} else if ($values['gender']) {print 'checked';} ?> >
+          <input type="radio" name="gender" value="m" <?php if ($errors['gender']) {print 'class="error"';} else if ($values['gender'] == 'm') {print 'checked';} ?> >
           <label>
             Мужской
           </label>
-          <input type="radio" name="gender" value="w" <?php if ($errors['gender']) {print 'class="error"';} else if ($values['limbs']) {print 'checked';} ?> >
+          <input type="radio" name="gender" value="w" <?php if ($errors['gender']) {print 'class="error"';} else if ($values['gender'] == 'w') {print 'checked';} ?> >
           <label>
             Женский
           </label>
@@ -63,17 +63,17 @@
             Количество конечностей <br>
 			      <p <?php if ($errors['limbs']) {print 'class="error"';} ?> > <?php if ($messages['limbs']) print $messages['limbs']; ?> </p>
           </label>
-          <input type="radio" name="limbs" value=0 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs']) {print 'checked';} ?> >
+          <input type="radio" name="limbs" value=0 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs'] == 0) {print 'checked';} ?> >
           <label>0</label>
-          <input type="radio" name="limbs" value=1 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs']) {print 'checked';} ?> >
+          <input type="radio" name="limbs" value=1 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs'] == 1) {print 'checked';} ?> >
           <label>1</label>
-          <input type="radio" name="limbs" value=2 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs']) {print 'checked';} ?> >
+          <input type="radio" name="limbs" value=2 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs'] == 2) {print 'checked';} ?> >
           <label>2</label>
-          <input type="radio" name="limbs" value=3 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs']) {print 'checked';} ?> >
+          <input type="radio" name="limbs" value=3 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs'] == 3) {print 'checked';} ?> >
           <label>3</label>
-          <input type="radio" name="limbs" value=4 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs']) {print 'checked';} ?> >
+          <input type="radio" name="limbs" value=4 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs'] == 4) {print 'checked';} ?> >
           <label>4</label>
-          <input type="radio" name="limbs" value=5 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs']) {print 'checked';} ?> >
+          <input type="radio" name="limbs" value=5 <?php if ($errors['limbs']) {print 'class="error"';} else if ($values['limbs'] == 5) {print 'checked';} ?> >
           <label>5+</label>
         </li>
         <li>
@@ -82,9 +82,9 @@
             <p <?php if ($errors['superpowers']) {print 'class="error"';} ?> > <?php if ($messages['superpowers']) print $messages['superpowers']; ?> </p>
           </label>
           <select multiple="true" name="superpowers[]" class="select-list">
-              <option value="Бессмертие" <?php if (in_array("Бессмертие", $superpowers)) {print 'checked';} ?> >Бессмертие</option>
-              <option value="Прохождение сквозь стены" <?php if (in_array("Прохождение сквозь стены", $superpowers)) {print 'checked';} ?> >Прохождение сквозь стены</option>
-              <option value="Левитация" <?php if (in_array("Левитация", $superpowers)) {print 'checked';} ?> >Левитация</option>
+              <option value="Бессмертие" <?php if (in_array("Бессмертие", $superpowers)) {print 'selected';} ?> >Бессмертие</option>
+              <option value="Прохождение сквозь стены" <?php if (in_array("Прохождение сквозь стены", $superpowers)) {print 'selected';} ?> >Прохождение сквозь стены</option>
+              <option value="Левитация" <?php if (in_array("Левитация", $superpowers)) {print 'selected';} ?> >Левитация</option>
           </select>
         </li>
         <li>
@@ -95,7 +95,7 @@
         </li>
         <li>
 		      <p <?php if ($errors['checkbox']) {print 'class="error"';} ?> > <?php if ($messages['checkbox']) print $messages['checkbox']; ?> </p>
-          <input type="checkbox" name="checkbox" <?php if ($errors['checkbox']) {print 'class="error"';} ?> >
+          <input type="checkbox" name="checkbox" <?php if ($errors['checkbox']) {print 'class="error"';} ?> <?php if ($values['checkbox']) {print 'checked';} ?> >
           <label>
             С контрактом ознакомлен
           </label>
