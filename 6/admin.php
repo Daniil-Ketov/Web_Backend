63b061fadda94602111c9de2d2a6f1e0b940d7f4
Daniil-Ketov <90641989+Daniil-Ketov@users.noreply.github.com>
2022-06-01 15:42:14 +0300

    Update admin.php

--- 6/admin.php
+++ 6/admin.php
@@ -50,9 +50,9 @@ print('Вы успешно авторизовались и видите защи
         print("<th>".$superpowers."</th><th>".$r['bio']."</th>");
         ?>
     <th><form method="POST">
-        <input style="display: none;" type="radio" name="id" value="<?php print($r['id']); ?>" checked>
-        <input  type="submit" name="edit" class="button" value="edit">
-        <input  type="submit" name="delete" class="button" value="delete"></form></th></tr>
+        <input  type="hidden" name="id" value="<?php print($r['id']); ?>" checked />
+        <input  type="submit" name="edit" class="button" value="edit" />
+        <input  type="submit" name="delete" class="button" value="delete" /></form></th></tr>
 <?php
     }
   }
